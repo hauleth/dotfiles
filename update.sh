2eed0f139c21d0c74eb6f2b6bf7cb7d0959b75df
Łukasz Niemier <lukasz@niemier.pl>
2014-05-14 12:29:00 +0200

    Update config

--- update.sh
+++ update.sh
@@ -2,6 +2,10 @@
 
 DIR="$(dirname "$0")"
 
-GIT_WORK_TREE="$DIR" git submodules foreach git pull && \
-  git add -A "$DIR/vim/bundle" && \
-  git commit -m "Update plugins $(date +%F)"
+echo "Update files in: $DIR"
+
+export GIT_DIR="$DIR/.git"
+export GIT_WORK_TREE="$DIR"
+git submodule foreach git pull
+git add -A "$DIR/vim/bundle"
+git commit -m "Update plugins $(date +%F)"
