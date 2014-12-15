8c29a467083947567fb157fda00246ca793bf90b
Łukasz Niemier <lukasz@niemier.pl>
2014-12-15 12:10:41 +0100

    Update ./update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-old="$(pwd)"
-
 dir="$(dirname "$0")"
-
 echo "Update files in: $dir"
 
-cd $dir
-
-git submodule foreach git pull &&\
-  git add -A "$dir/vim/bundle" &&\
+cd "$dir"
+peru reup &&\
+  git add -A "peru.yaml" &&\
   git commit -m "Update plugins $(date +%F)"
+cd "$OLDPWD"
