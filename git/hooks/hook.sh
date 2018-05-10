14a9b52946a649d43d79510b309b9fb69d705235
Łukasz Niemier <lukasz@niemier.pl>
2018-05-10 17:51:07 +0200

    Improve hook handler

--- git/hooks/hook.sh
+++ git/hooks/hook.sh
@@ -4,15 +4,29 @@ hooks=(post-checkout post-commit post-merge pre-commit pre-push prepare-commit-m
 
 usage() {
     echo "$0 [install|help]"
+    echo 'Version 0.1.0'
+    echo 'Copyright (c) Łukasz Niemier <opensource@niemier.pl>'
     echo
-    echo "Simple git hooks management system"
+    echo 'Simple git hooks management system'
+    echo
+    echo 'install [hook_dir]'
+    echo '    Installs hooks for given repo.'
+    echo
+    echo '    [hook_dir] - directory where install hooks, defaults to .git/hooks'
+    echo '                 in current Git working directory'
+    echo 'help'
+    echo '    display this message'
+    echo
+    echo 'To use this as default set of hooks when creating new repo then:'
+    echo
+    echo "    1. Run '$0 ~/.githooks'"
+    echo '    2. Run 'git config --global core.hooksPath ~/.githooks''
     echo
 }
 
 install() {
     source="${BASH_SOURCE[0]}"
-    DIR="${1:-"$(git rev-parse --show-toplevel)"}"
-    HOOK_DIR="${2:-"$DIR/.git/hooks"}"
+    HOOK_DIR="${1:-"$(git rev-parse --show-toplevel)/.git/hooks"}"
 
     echo "Install handler"
     echo
@@ -34,9 +48,6 @@ hook() {
     script="$(basename "$0")"
     PLUG_DIRS=("$(command git config --get hooks.path)" "$GIT_DIR/hooks/hooks.d")
 
-    echo "## Hook $script"
-    echo
-
     test -d "$GIT_DIR"/rebase-merge -o -d "$GIT_DIR"/rebase-apply && exit 0
 
     for dir in "${PLUG_DIRS[@]}"
@@ -44,11 +55,12 @@ hook() {
         if [ -d "$dir" ]
         then
             find "$dir" -depth 2 -and -name "$script" -print0 2>/dev/null \
-                | xargs -t0 -n1 -I_ sh -c '[ -x "$1" ] && printf "\n###### %s\n" "$1" && "$@" && echo "ok"' -- _ "$@"
+                | xargs -0 -n1 -I_ sh -c 'if [ -x "$1" ]; then printf "\n###### %s\n" "$1" && "$@" && echo "ok"; fi' -- _ "$@"
+            retval="$?"
 
-            if [ ! "$?" -eq 0 ]
+            if [ ! "$retval" -eq 0 ]
             then
-                return "$?"
+                return "$retval"
             fi
         fi
     done
@@ -58,9 +70,9 @@ case "$1" in
     install)
         shift
         install
-        exit 0
+        exit
         ;;
-    help)
+    help|-h|--help|version|-v|-V|--version)
         shift
         usage
         exit
